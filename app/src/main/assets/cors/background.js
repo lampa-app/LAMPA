b19b9d2727ce4d6a8940e3f2896aacf3eb7cb7f5
nikk gitanes <tsynik@gmail.com>
2023-05-16 23:19:44 +0300

    update cors ext
    just test

--- app/src/main/assets/cors/background.js
+++ app/src/main/assets/cors/background.js
@@ -2,32 +2,93 @@
 /**
  * Force Access-Control-Allow-Origin
  */
+var LAMPA_URL = "http://lampa.mx";
+
+function originWithId(header) {
+  return (
+    header.name.toLowerCase() === "origin" &&
+    (header.value.indexOf("moz-extension://") === 0 ||
+      header.value.indexOf("chrome-extension://") === 0)
+  );
+}
+
+function origin(header) {
+  return header.name.toLowerCase() === "origin";
+}
+
+function sec(header) {
+  return header.name.toLowerCase().startsWith("sec-");
+}
+
 function onHeadersReceived(resp) {
-  var len = resp.responseHeaders.length;
-  //console.log(`***** Headers len: [${len}] Response: ${resp.toString()}`);
-  while(--len) {
-    //console.log(`***** ${resp.responseHeaders[len].name} ${resp.responseHeaders[len].value}`);
-    if(resp.responseHeaders[len].name.toLowerCase() === "access-control-allow-credentials") {
+  let len = resp.responseHeaders.length;
+  let cred = false;
+  console.log(
+    `***** onHeadersReceived url: [${resp.url}] id[${
+      resp.requestId
+    }] headers[${len}] ${JSON.stringify(resp.responseHeaders)}`
+  );
+  while (--len) {
+    if (
+      resp.responseHeaders[len].name.toLowerCase() ===
+      "access-control-allow-credentials"
+    ) {
+      cred = true;
       break;
     }
-    if(resp.responseHeaders[len].name.toLowerCase() === "access-control-allow-origin") {
-      resp.responseHeaders[len].value = "*";
+    if (
+      resp.responseHeaders[len].name.toLowerCase() ===
+      "access-control-allow-origin"
+    ) {
+      resp.responseHeaders[len].value = LAMPA_URL;
       break;
     }
   }
-  if (len === 0) { // if we didn't find it len will be zero
+  if (len === 0) {
+    // if we didn't find it len will be zero
     resp.responseHeaders.push({
-      'name': 'Access-Control-Allow-Origin',
-      'value': '*'
+      name: "Access-Control-Allow-Origin",
+      value: LAMPA_URL,
+    });
+  } else if (cred) {
+    resp.responseHeaders.push({
+      name: "Access-Control-Allow-Origin",
+      value: LAMPA_URL,
     });
   }
-  return {responseHeaders: resp.responseHeaders};
+  return {
+    responseHeaders: resp.responseHeaders,
+  };
 }
 
 browser.webRequest.onHeadersReceived.addListener(
   onHeadersReceived,
-  {urls: ['*://*/*'],
-  /* TYPES: "main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other" */
-  types: ['xmlhttprequest']},
-  ['blocking', 'responseHeaders']
+  {
+    urls: ["<all_urls>"],
+    /* TYPES: "main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other" */
+    types: ["xmlhttprequest"],
+  },
+  ["blocking", "responseHeaders"]
+);
+
+browser.webRequest.onBeforeSendHeaders.addListener(
+  (details) => {
+    let headers = details.requestHeaders.filter(
+      (x) => !originWithId(x) && !sec(x)
+    );
+    console.log(
+      `***** onBeforeSendHeaders id[${details.requestId}] orig headers[${
+        details.requestHeaders.length
+      }] ${JSON.stringify(headers)}`
+    );
+    return {
+      requestHeaders: headers,
+    };
+  },
+  {
+    urls: ["<all_urls>"],
+    /* TYPES: "main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other" */
+    types: ["xmlhttprequest"],
+  },
+  ["blocking", "requestHeaders"]
 );
