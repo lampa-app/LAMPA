4be07b60114361752ea3249f9f53fdb92ae2dbde
nikk gitanes <tsynik@gmail.com>
2025-03-19 22:09:33 +0300

    refactor Scheduler and fix duplicate updates

--- app/src/main/java/top/rootu/lampa/sched/CardJobService.kt
+++ app/src/main/java/top/rootu/lampa/sched/CardJobService.kt
@@ -3,22 +3,44 @@ package top.rootu.lampa.sched
 import android.app.job.JobParameters
 import android.app.job.JobService
 import android.os.Build
+import android.util.Log
 import androidx.annotation.RequiresApi
+import top.rootu.lampa.BuildConfig
 import kotlin.concurrent.thread
 
 @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
 class CardJobService : JobService() {
-    private var complete = false
+    private var isJobComplete = false
+
+    /**
+     * Called when the job is started.
+     *
+     * @param params The parameters for the job.
+     * @return True if the job is running on a separate thread, false otherwise.
+     */
     override fun onStartJob(params: JobParameters?): Boolean {
-        complete = false
+        isJobComplete = false
+
+        // Start a background thread to update cards
         thread {
-            Scheduler.updateCards(true)
-            complete = true
-        }.join(500)
-        return !complete
+            if (BuildConfig.DEBUG) Log.i("Scheduler", "CardJobService call updateCards(sync: true)")
+            Scheduler.updateCards(true) // Update cards with sync enabled
+            isJobComplete = true
+            jobFinished(params, false) // Notify the system that the job is complete
+        }
+
+        // Return true to indicate that the job is running on a separate thread
+        return true
     }
 
+    /**
+     * Called when the job is stopped.
+     *
+     * @param params The parameters for the job.
+     * @return True if the job should be rescheduled, false otherwise.
+     */
     override fun onStopJob(params: JobParameters?): Boolean {
-        return !complete
+        // Return true to reschedule the job if it was stopped prematurely
+        return !isJobComplete
     }
 }
--- app/src/main/java/top/rootu/lampa/sched/Scheduler.kt
+++ app/src/main/java/top/rootu/lampa/sched/Scheduler.kt
@@ -21,65 +21,123 @@ import java.util.concurrent.TimeUnit
 import kotlin.concurrent.thread
 
 object Scheduler {
-    private const val cardsJobId = 0
+    private const val CARDS_JOB_ID = 0
     private val lock = Any()
     private var isUpdate = false
 
-    fun scheduleUpdate(sync: Boolean) {
-        if (!isAndroidTV)
-            return
+    /**
+     * Schedules periodic updates for cards.
+     *
+     * @param sched Whether to schedule updates or perform a one-shot update.
+     */
+    fun scheduleUpdate(sched: Boolean) {
+        if (!isAndroidTV) return
 
-        if (BuildConfig.DEBUG)
-            Log.i("*****", "Scheduler: scheduleUpdate(sync: $sync)")
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
-            jobScheduler(sync)
-        else
+        if (BuildConfig.DEBUG) Log.i("Scheduler", "scheduleUpdate(sched: $sched)")
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            jobScheduler(sched)
+        } else {
             alarmScheduler()
-    }
-
-    @TargetApi(Build.VERSION_CODES.M)
-    private fun jobScheduler(sync: Boolean) {
-        val context = App.context
-        thread {
-            updateCards(sync)
         }
-        val builder = JobInfo.Builder(cardsJobId, ComponentName(context, CardJobService::class.java))
-        builder.setPeriodic(TimeUnit.MINUTES.toMillis(15))
-            .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
-            .setRequiresDeviceIdle(false)
-            .setRequiresCharging(false)
-
-        context.getSystemService(JobScheduler::class.java)?.schedule(builder.build())
     }
 
+    /**
+     * Uses JobScheduler to schedule periodic updates (for Android M and above).
+     *
+     * @param sched Whether to schedule updates or perform a one-shot update.
+     */
+    @TargetApi(Build.VERSION_CODES.M)
+    private fun jobScheduler(sched: Boolean) {
+        val context = App.context
+
+        if (sched) {
+            // Configure the JobScheduler for periodic updates
+            val jobScheduler = context.getSystemService(JobScheduler::class.java)
+            val jobInfo = JobInfo.Builder(
+                CARDS_JOB_ID,
+                ComponentName(context, CardJobService::class.java)
+            ).apply {
+                setPeriodic(TimeUnit.MINUTES.toMillis(15)) // Schedule every 15 minutes
+                setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY) // Require any network
+                setRequiresDeviceIdle(false) // Don't require device to be idle
+                setRequiresCharging(false) // Don't require device to be charging
+            }.build()
+            if (BuildConfig.DEBUG) Log.i(
+                "Scheduler",
+                "jobScheduler schedule periodic updates with CardJobService"
+            )
+            jobScheduler?.schedule(jobInfo)
+        } else { // Perform a one-shot update in a background thread
+            thread {
+                if (BuildConfig.DEBUG) Log.i(
+                    "Scheduler",
+                    "jobScheduler call updateCards(sync: $sched)"
+                )
+                updateCards(sched)
+            }
+        }
+    }
+
+    /**
+     * Uses AlarmManager to schedule periodic updates (for older Android versions).
+     */
     private fun alarmScheduler() {
         val context = App.context
-        val pendingIntent = Intent(context, CardAlarmManager::class.java)
         val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        val alarmIntent = PendingIntent.getService(context, cardsJobId, pendingIntent, 0)
 
-        alarmManager.cancel(alarmIntent)
+        // Create a PendingIntent for the alarm
+        val pendingIntent = Intent(context, CardAlarmManager::class.java).let { intent ->
+            PendingIntent.getService(context, CARDS_JOB_ID, intent, 0)
+        }
+
+        // Cancel any existing alarms
+        alarmManager.cancel(pendingIntent)
+
+        // Schedule a repeating alarm
         alarmManager.setInexactRepeating(
-            AlarmManager.ELAPSED_REALTIME_WAKEUP,
-            SystemClock.elapsedRealtime(),
-            AlarmManager.INTERVAL_HOUR,
-            alarmIntent
+            AlarmManager.ELAPSED_REALTIME_WAKEUP, // Wake up the device
+            SystemClock.elapsedRealtime(), // Start time
+            AlarmManager.INTERVAL_HOUR, // Repeat every hour
+            pendingIntent // PendingIntent to trigger
         )
     }
 
+    /**
+     * Updates the cards data.
+     *
+     * @param sync Whether to sync data with the server.
+     */
     @RequiresApi(Build.VERSION_CODES.KITKAT)
     fun updateCards(sync: Boolean) {
         synchronized(lock) {
-            if (isUpdate)
-                return
+            if (BuildConfig.DEBUG) Log.i(
+                "Scheduler",
+                "updateCards sync: $sync isUpdate: $isUpdate."
+            )
+            if (isUpdate) {
+                if (BuildConfig.DEBUG) Log.i("Scheduler", "updateCards isUpdate = true, cancel.")
+                return // Prevent multiple updates at the same time
+            }
             isUpdate = true
         }
 
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O)
-            RecsService.updateRecs()
-        else
-            LampaChannels.update(sync)
-        isUpdate = false
+        try {
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+                if (BuildConfig.DEBUG) Log.i(
+                    "Scheduler",
+                    "updateCards call RecsService.updateRecs()."
+                )
+                RecsService.updateRecs() // Update recommendations for older versions
+            } else {
+                if (BuildConfig.DEBUG) Log.i(
+                    "Scheduler",
+                    "updateCards call LampaChannels.update($sync)."
+                )
+                LampaChannels.update(sync) // Update channels for newer versions
+            }
+        } finally {
+            isUpdate = false // Reset the update flag
+        }
     }
-
 }
